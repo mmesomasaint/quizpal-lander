0f83715227c28d55966511b5d0a99d0d3e33ee4b
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-15 04:04:07 +0000

    Feat: Delete all unwanted lines

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Alegreya } from 'next/font/google'
+
+const alegreya = Alegreya({ subsets: ['latin'] })
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={alegreya.className}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
