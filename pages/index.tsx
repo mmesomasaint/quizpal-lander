561c83f52cc74f344ed410536a42f1fe1c49effd
webmonk <104903171+mmesomasaint@users.noreply.github.com>
2023-04-16 06:10:31 +0000

    Fix: Complete showcase design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import Link from 'next/link'
 import Head from 'next/head'
 import { GiHamburgerMenu } from 'react-icons/gi'
+import { HiArrowSmRight } from 'react-icons/hi'
 
 export default function Home() {
   return (
@@ -56,8 +57,17 @@ export default function Home() {
         </div>
       </header>
       <main className='mt-32'>
-        <div className='text-center text-8xl font-bold w-[80%] mx-auto'>
-          Draft, Share and Host your quiz with ease.
+        <div className='text-center'>
+          <h1 className='text-6xl md:text-8xl font-bold mb-6 w-[90%] sm:w-[85%] lg:w-[65%] mx-auto'>
+            Draft, Share and Host your quiz with ease.
+          </h1>
+          <h3 className='text-xl md:text-3xl font-medium mb-6 w-[80%] sm:w-[55%] xl:w-[35%] mx-auto'>
+            Quizpal is an online workshop for easier, effective quiz handling.
+          </h3>
+          <button className='inline-flex justify-start items-center gap-2 py-2 px-3 bg-black rounded-md'>
+            <span className='text-sm lg:text-base font-bold leading-none text-white'>Get Quizpal free</span>
+            <HiArrowSmRight className='text-base lg:text-xl text-white' />
+          </button>
         </div>
       </main>
     </>
