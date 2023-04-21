dee6713407ef192e241789a6069affe4c9499ecd
Mmesoma Saint <mmesomasaint@gmail.com>
2023-04-21 06:17:45 +0100

    Fix: reWrite the showcase copy

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,10 +14,10 @@ export default function Home() {
       <main className='mt-32'>
         <div className='text-center'>
           <h1 className='text-6xl md:text-8xl font-bold mb-6 w-[90%] sm:w-[85%] lg:w-[65%] mx-auto'>
-            Draft, Share and Host your quiz with ease.
+            Draft, Share &amp; Host your quiz. With ease.
           </h1>
           <h3 className='text-xl md:text-3xl font-medium mb-6 w-[80%] sm:w-[55%] xl:w-[35%] mx-auto'>
-            Quizpal is an online workshop for easier, effective quiz handling.
+            Quizpal is an online workshop for seamless, effective quiz handling.
           </h3>
           <button className='inline-flex justify-start items-center gap-2 py-2 px-3 bg-black rounded-md'>
             <span className='text-sm sm:text-base font-bold leading-none text-white'>Get Quizpal free</span>
